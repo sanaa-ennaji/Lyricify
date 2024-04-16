d8a9e2cd4cdc1261c243f05c2c71e4e4ac973499
sanaa <sanaaennaji93@gmail.com>
2024-04-16 11:23:45 +0100

    add vue js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/songs.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+</template>
+
+
+
+
+
+
+
+
